1148e9364ca96717740cbf865d113820fd0a51b1
EsdrasMukuku <esdrasbam0@gmail.com>
2024-04-17 15:52:25 -0500

    header
    finished header layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+  return (
+    <div className="bg-red text-white text-center text-[0.90375rem] flex justify-center items-center h-[55px] p-0 gap-1 font-medium">
+      <img src="../../public/assets/Fill 213.png" alt=""className="h-[24px] w-[24px]"/>
+      <p>my travel journal.</p>
+    </div>
+  )
+}
+
+export default Header
